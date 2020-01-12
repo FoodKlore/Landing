9b1e7b9ca02a7f274abebc3758e784561e1d182e
eduarmreyes <eduar.mreyes@gmail.com>
2020-01-11 20:45:31 -0800

    add background image to experts section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "src/components/layout";
+import Layout from "src/components/layout"
 import Image from "src/components/image"
 import SEO from "src/components/seo"
 import Banner from "src/components/banner"
 import ShowCase from "src/components/showcase"
+import Experts from "src/components/experts"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Banner />
     <ShowCase />
+    <Experts />
   </Layout>
 )
 
