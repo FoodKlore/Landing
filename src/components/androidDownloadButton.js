a8e9bce166c1a2cbbd7b4abe5067c1d1d8a5177a
eduarmreyes <eduar.mreyes@gmail.com>
2020-01-19 10:27:01 -0800

    fix eslint errors

--- src/components/androidDownloadButton.js
+++ src/components/androidDownloadButton.js
@@ -2,8 +2,13 @@ import React from "react"
 import AndroidDownloadImage from "src/images/AndroidBadgeBanner.svg"
 
 const AndroidDownloadButton = props => (
-  <a href="https://example.com" target="_blank" {...props}>
-    <img src={AndroidDownloadImage}></img>
+  <a
+    href="https://example.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    {...props}
+  >
+    <img alt="Download the App for Android" src={AndroidDownloadImage}></img>
   </a>
 )
 
--- src/components/showcase.js
+++ src/components/showcase.js
@@ -103,19 +103,6 @@ const ShowCaseTitleWrapper = styled.div`
   }
 `
 
-const FkP = styled.p`
-  width: 489px;
-  height: 22px;
-  font-family: Montserrat;
-  font-size: 16px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.19;
-  letter-spacing: normal;
-  text-align: left;
-  color: #9292af;
-`
 const ShowCaseItemWrapper = styled.div`
   display: flex;
   justify-content: space-evenly;
