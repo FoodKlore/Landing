2e543c1d13b8d3c7040a16b162c66d70d5a8381e
eduarmreyes <eduar.mreyes@gmail.com>
2020-01-26 12:15:09 -0800

    add layout of how it works steps

--- src/components/howItWorks.js
+++ src/components/howItWorks.js
@@ -60,6 +60,136 @@ const HowItWorks = () => (
             Using our application is very easy and practical, just follow these
             steps
           </p>
+          <div style={{ display: "flex", marginBottom: "12px" }}>
+            <div>
+              <span
+                style={{
+                  display: "inline-flex",
+                  borderRadius: "100%",
+                  color: "#fff",
+                  width: "40px",
+                  height: "40px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: "15px",
+                  backgroundColor: "#37496d",
+                  fontWeight: "600",
+                  fontSize: "15px",
+                  border: "1px solid #37496d",
+                }}
+              >
+                1
+              </span>
+            </div>
+            <div>
+              <h4
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "600",
+                  color: "#3b3b3b",
+                  marginBottom: "16px",
+                }}
+              >
+                Download the App
+              </h4>
+              <p
+                style={{
+                  fontSize: "11px",
+                  lineHeight: "14px",
+                  color: "#3b3b3b",
+                }}
+              >
+                We are available on any operating system, just search on Google
+                Play or Apple Store
+              </p>
+            </div>
+          </div>
+          <div style={{ display: "flex", marginBottom: "12px" }}>
+            <div>
+              <span
+                style={{
+                  display: "inline-flex",
+                  borderRadius: "100%",
+                  color: "#37496d",
+                  width: "40px",
+                  height: "40px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: "15px",
+                  backgroundColor: "#fff",
+                  fontWeight: "600",
+                  fontSize: "15px",
+                  border: "1px solid #37496d",
+                }}
+              >
+                2
+              </span>
+            </div>
+            <div>
+              <h4
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "600",
+                  color: "#3b3b3b",
+                  marginBottom: "16px",
+                }}
+              >
+                Select cuisine's country
+              </h4>
+              <p
+                style={{
+                  fontSize: "11px",
+                  lineHeight: "14px",
+                  color: "#3b3b3b",
+                }}
+              >
+                Choose the culture flavor you want to eat and discover the variety of restaurant options that around you
+              </p>
+            </div>
+          </div>
+          <div style={{ display: "flex", marginBottom: "12px" }}>
+            <div>
+              <span
+                style={{
+                  display: "inline-flex",
+                  borderRadius: "100%",
+                  color: "#37496d",
+                  width: "40px",
+                  height: "40px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: "15px",
+                  backgroundColor: "#fff",
+                  fontWeight: "600",
+                  fontSize: "15px",
+                  border: "1px solid #37496d",
+                }}
+              >
+                3
+              </span>
+            </div>
+            <div>
+              <h4
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "600",
+                  color: "#3b3b3b",
+                  marginBottom: "16px",
+                }}
+              >
+                Enjoy
+              </h4>
+              <p
+                style={{
+                  fontSize: "11px",
+                  lineHeight: "14px",
+                  color: "#3b3b3b",
+                }}
+              >
+                Choose the restaurant you prefer, visit it and enjoy the experience of cultural flavor
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
