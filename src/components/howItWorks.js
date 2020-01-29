1f6af2d959d8da0fbb22f2de1b4aa7c15d9bb2b3
eduarmreyes <eduar.mreyes@gmail.com>
2020-01-28 22:15:40 -0800

    add how it works item component

--- src/components/howItWorks.js
+++ src/components/howItWorks.js
@@ -1,7 +1,76 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 
 import SignUp1 from "src/images/SignUp1x.png"
+
+const howItWorksData = [
+  {
+    id: "8d569978-6fe9-4382-b258-36d3aca59a8d",
+    step: 1,
+    title: "Download the App",
+    description:
+      "We are available on any Operating System, just Google us and you will find us on Apple or Play Store.",
+  },
+  {
+    id: "edf69fa2-17bc-4707-b138-596832bfa677",
+    step: 2,
+    title: "Select cuisine's country",
+    description:
+      "Choose the culture flavor you want to eat and discover the variety of restaurant options that around you.",
+  },
+  {
+    id: "14ac32ae-24c8-487a-9fa1-effa36dbebde",
+    step: 3,
+    title: "Enjoy",
+    description:
+      "Choose the restaurant you prefer, visit it and enjoy the experience of cultural flavor.",
+  },
+]
+
+const HowItWorksItem = props => (
+  <div key={props.element.id} style={{ display: "flex", marginBottom: "12px" }}>
+    <div>
+      <span
+        style={{
+          display: "inline-flex",
+          borderRadius: "100%",
+          width: "40px",
+          height: "40px",
+          justifyContent: "center",
+          alignItems: "center",
+          marginRight: "15px",
+          fontWeight: "600",
+          fontSize: "15px",
+          border: "1px solid #37496d",
+        }}
+        className={props.element.isActive ? "active" : "inactive"}
+      >
+        {props.element.step}
+      </span>
+    </div>
+    <div>
+      <h4
+        style={{
+          fontSize: "13px",
+          fontWeight: "600",
+          color: "#3b3b3b",
+          marginBottom: "16px",
+        }}
+      >
+        {props.element.title}
+      </h4>
+      <p
+        style={{
+          fontSize: "11px",
+          lineHeight: "14px",
+          color: "#3b3b3b",
+        }}
+      >
+        {props.element.description}
+      </p>
+    </div>
+  </div>
+)
 
 const HowItWorks = () => (
   <HowItWorksSection
@@ -62,136 +131,9 @@ const HowItWorks = () => (
             Using our application is very easy and practical, just follow these
             steps
           </p>
-          <div style={{ display: "flex", marginBottom: "12px" }}>
-            <div>
-              <span
-                style={{
-                  display: "inline-flex",
-                  borderRadius: "100%",
-                  color: "#fff",
-                  width: "40px",
-                  height: "40px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginRight: "15px",
-                  backgroundColor: "#37496d",
-                  fontWeight: "600",
-                  fontSize: "15px",
-                  border: "1px solid #37496d",
-                }}
-              >
-                1
-              </span>
-            </div>
-            <div>
-              <h4
-                style={{
-                  fontSize: "13px",
-                  fontWeight: "600",
-                  color: "#3b3b3b",
-                  marginBottom: "16px",
-                }}
-              >
-                Download the App
-              </h4>
-              <p
-                style={{
-                  fontSize: "11px",
-                  lineHeight: "14px",
-                  color: "#3b3b3b",
-                }}
-              >
-                We are available on any operating system, just search on Google
-                Play or Apple Store
-              </p>
-            </div>
-          </div>
-          <div style={{ display: "flex", marginBottom: "12px" }}>
-            <div>
-              <span
-                style={{
-                  display: "inline-flex",
-                  borderRadius: "100%",
-                  color: "#37496d",
-                  width: "40px",
-                  height: "40px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginRight: "15px",
-                  backgroundColor: "#fff",
-                  fontWeight: "600",
-                  fontSize: "15px",
-                  border: "1px solid #37496d",
-                }}
-              >
-                2
-              </span>
-            </div>
-            <div>
-              <h4
-                style={{
-                  fontSize: "13px",
-                  fontWeight: "600",
-                  color: "#3b3b3b",
-                  marginBottom: "16px",
-                }}
-              >
-                Select cuisine's country
-              </h4>
-              <p
-                style={{
-                  fontSize: "11px",
-                  lineHeight: "14px",
-                  color: "#3b3b3b",
-                }}
-              >
-                Choose the culture flavor you want to eat and discover the variety of restaurant options that around you
-              </p>
-            </div>
-          </div>
-          <div style={{ display: "flex", marginBottom: "12px" }}>
-            <div>
-              <span
-                style={{
-                  display: "inline-flex",
-                  borderRadius: "100%",
-                  color: "#37496d",
-                  width: "40px",
-                  height: "40px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginRight: "15px",
-                  backgroundColor: "#fff",
-                  fontWeight: "600",
-                  fontSize: "15px",
-                  border: "1px solid #37496d",
-                }}
-              >
-                3
-              </span>
-            </div>
-            <div>
-              <h4
-                style={{
-                  fontSize: "13px",
-                  fontWeight: "600",
-                  color: "#3b3b3b",
-                  marginBottom: "16px",
-                }}
-              >
-                Enjoy
-              </h4>
-              <p
-                style={{
-                  fontSize: "11px",
-                  lineHeight: "14px",
-                  color: "#3b3b3b",
-                }}
-              >
-                Choose the restaurant you prefer, visit it and enjoy the experience of cultural flavor
-              </p>
-            </div>
-          </div>
+          {howItWorksData.map(element => (
+            <HowItWorksItem key={element.id} element={element}></HowItWorksItem>
+          ))}
         </div>
       </div>
     </div>
@@ -199,6 +141,10 @@ const HowItWorks = () => (
 )
 
 const HowItWorksSection = styled.section`
+  .active {
+    background-color: "#37496d"
+    color: "#fff"
+  }
   h1,
   p {
   }
