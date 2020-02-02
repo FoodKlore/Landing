48867bad603ac11b6378e979a6fab5dbfa1f0820
Alejandro Mejia <aalejandromr@AlejandrosAir2.lan1>
2020-02-01 19:56:02 -0500

    Fixes experts for mobile

--- src/components/experts.js
+++ src/components/experts.js
@@ -5,48 +5,32 @@ import AndroidDownloadButton from "./androidDownloadButton"
 import IOSDownloadButton from "./iOSDownloadButton"
 
 const Experts = () => (
-  <ExpertsSection
-    style={{
-      maxWidth: "1080px",
-      width: "1080px",
-      margin: "0 auto",
-    }}
-  >
-    <div
-      style={{
-        padding: "175px 220px",
-        backgroundImage: `url(${
-          ExpertsSectionBackground().expertsSectionBackgroundImage
-            .childImageSharp.fluid.src
-        }), url(${
-          ExpertsSectionBackground().expertsSectionBackgroundImageRotated
-            .childImageSharp.fluid.src
-        })`,
-        backgroundColor: `#e83b54`,
-        backgroundPosition: `bottom left, top right`,
-        backgroundRepeat: `no-repeat`,
-        borderRadius: "50px 0",
-      }}
-    >
+  <ResponsiveExpertsSection>
+    <ResponsiveBackground backgrounds={ExpertsSectionBackground()}>
       <p>
         Different experts of every country working with us who verify each meal
         and guarantee that our allies have the ability to make you experience
         flavors from other countries.
       </p>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
+      <ResponsiveDownloadButtons>
         <AndroidDownloadButton style={{ marginRight: "15px" }} />
         <IOSDownloadButton style={{ padding: "9px 0" }} />
-      </div>
-    </div>
-  </ExpertsSection>
+      </ResponsiveDownloadButtons>
+    </ResponsiveBackground>
+  </ResponsiveExpertsSection>
 )
 
-const ExpertsSection = styled.section`
+const ResponsiveDownloadButtons = styled.div`
+  display: flex;
+  justify-content: center;
+  @media only screen and (max-width: 699px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const ResponsiveExpertsSection = styled.section`
   h1,
   p {
     color: #fff;
@@ -55,6 +39,53 @@ const ExpertsSection = styled.section`
     font-size: 20px;
     line-height: 25px;
   }
+  max-width: 1080px;
+  width: 1080px;
+  margin: 0 auto;
+
+  @media only screen and (max-width: 699px) {
+    width: 100%;
+    max-width: 100%;
+    padding: 0px 12px 0px 12px;
+    p {
+      width: 235px;
+      height: 254px;
+      font-family: Montserrat;
+      font-size: 18px;
+      font-weight: 600;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: 1.67;
+      letter-spacing: normal;
+      text-align: center;
+      color: #ffffff;
+    }
+  }
+`
+const ResponsiveBackground = styled.div`
+  padding: 175px 220px;
+  background-color: #e83b54;
+  background-position: bottom left, top right;
+  background-repeat: no-repeat;
+  border-radius: 50px 0;
+
+  @media only screen and (max-width: 699px) {
+    padding: 175px 0px;
+    height: 776px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+  }
+
+  background-image: url(${prop => {
+      return prop.backgrounds.expertsSectionBackgroundImage.childImageSharp
+        .fluid.src
+    }}),
+    url(${prop => {
+      return prop.backgrounds.expertsSectionBackgroundImageRotated
+        .childImageSharp.fluid.src
+    }});
 `
 
 export default Experts
