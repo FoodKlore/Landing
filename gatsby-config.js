4fd50e96d4797ccdf4bf3941f8aaa84c7c8736c0
eduarmreyes <eduar.mreyes@gmail.com>
2020-03-02 22:45:37 -0800

    Add hotjar plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,12 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-root-import`,
+    {
+      resolve: `gatsby-plugin-hotjar`,
+      options: {
+        id: `1710898`,
+        sv: `6`,
+      },
+    },
   ],
 }
